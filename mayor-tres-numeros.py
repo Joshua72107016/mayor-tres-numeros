502918976a79b70c7ebd6795e4d760d609ab379d
72107016 <72107016@continental.edu.pe>
2025-03-26 11:41:20 -0500

    Se añadió una funcion para intercambiar los valores

--- mayor-tres-numeros.py
+++ mayor-tres-numeros.py
@@ -1,24 +1,23 @@
 """
 Determina el mayor de tres numeros ingresados por el teclado
 """
+def intercambiar_valores(numero1, numero2):
+    temporal = numero1
+    numero1 = numero2
+    numero2 = temporal
+    return numero1,numero2
+
 numero1=int(input("Ingrese el primer  numero:"))
 numero2=int(input("Ingrese el segundo numero: "))
 numero3=int(input("Ingrese el tercer  numero: "))
 
 if numero1>numero2:
-    temporal=numero1
-    numero1=numero2
-    numero3=temporal
+  numero1,numero2 = intercambiar_valores(numero1, numero2)
 
 if numero2>numero3:
-    temporal=numero2
-    numero2=numero3
-    numero2=temporal
-
+    numero2, numero3 = intercambiar_valores(numero2, numero3)
 if numero1>numero2:
-    temporal=numero1
-    numero1=numero2
-    numero2=temporal
+    numero1,numero2 = intercambiar_valores(numero1, numero2)
 
 print(f"numeros ordenados: {numero1},{numero2},{numero3}")
 print(f"El mayor es {numero3}")
